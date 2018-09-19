59cfd2bf4dd9c814bf3aec1019c89c6d7f6f7ac3
George <george@ubuntu.ubuntu-domain>
2018-09-19 04:59:14 +0000

    mongodb search

--- API/MVCSolution/Controllers/NewClass.cs
+++ API/MVCSolution/Controllers/NewClass.cs
@@ -5,31 +5,68 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Web;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Threading.Tasks;
+using MongoDB.Driver.GeoJsonObjectModel;
 
 namespace MVCSolution.Controllers
 {
-    public class NewClass
-    {
-        
-        private ApplicationDbContext db = new ApplicationDbContext();
+	public class NewClass
+	{
 
-        public NewClass()
-        {
-           
-        }
-        public void FunctionThatSolvesThings(string id)
-        {
+		private ApplicationDbContext db = new ApplicationDbContext();
 
-            var user = db.Users.Where(u => u.OBD_Id == id).FirstOrDefault();
+		public NewClass()
+		{
+
+		}
+		public void FunctionThatSolvesThings(string id)
+		{
+			Console.WriteLine("new class");
+
+			var user = db.Users.Where(u => u.OBD_Id == id).FirstOrDefault();
+
+			string target = user.UserName;
+
+			Console.WriteLine(id);
+			Console.WriteLine(target);
+
+			//ChatHub ch = new ChatHub();
+			//ch.Send("love you", target);
+		}
+		public Parkinglot Location(double lat, double lng)
+		{
+			
+
+			var client = new MongoClient();
+			var mongo_db = client.GetDatabase("GeoDB");
+			var collection = mongo_db.GetCollection<Parkinglot>("parkinglot");
+														  
+			try
             
-            string target = user.UserName;
+			{
+				       
+                var point = GeoJson.Point(GeoJson.Geographic(lat, lng));
+				var query = new FilterDefinitionBuilder<Parkinglot>().GeoIntersects(tag => tag.location, point);
 
-            Debug.WriteLine(id);
-            Debug.WriteLine(target);
-
-            //ChatHub ch = new ChatHub();
-            //ch.Send("love you", target);
-        }
-    
+				var plots = collection.Find<Parkinglot>(query).FirstOrDefault();
+				return plots;
+			
+			}    
+			catch (Exception e){
+				Console.WriteLine(e);
+			}
+			return null;
+		}
     }
 }
+
+/*db.parkinglot.find({coordinates:
+#                  {$geoIntersects:
+#                      {$geometry:{ "type" : "Point",
+#                           "coordinates" : [57.047748, 9.926616] }
+#                       }
+#                   }
+#              })
+*/
